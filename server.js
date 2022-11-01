2b62b9d59d56284aab5bd6348e96bf74f53a1119
Jessica Almand <jlalmand27@gmail.com>
2022-10-31 22:10:48 -0600

    Created get requests for HTML files.

--- server.js
+++ server.js
@@ -11,13 +11,23 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+//HTML Routes
+
 //GET /notes should return the notes.html file.
+//testing index.html get request 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
-
-app.get('/notes', (req, res) => res.json(data));
+//GET * should return the index.html file.
+//app.get('*', (req, res) => {
+    //res.sendFile(path.join(__dirname, 'index.html'));
+//});
+//GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => res.json(data));
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
