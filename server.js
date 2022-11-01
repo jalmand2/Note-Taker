72ed3525a2670cdd152589b6c7039a1bd579193d
Jessica Almand <jlalmand27@gmail.com>
2022-10-31 19:06:48 -0600

    Added get request for notes to server.js

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index');
+const data = require('./db/db.json');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+//GET /notes should return the notes.html file.
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+app.get('/notes', (req, res) => res.json(data));
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
