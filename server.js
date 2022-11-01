b384bc3b05b9bc33029557ddcac46da6a237c963
Jessica Almand <jlalmand27@gmail.com>
2022-11-01 00:02:37 -0600

    Tried to create post request for notes, didn't have any luck. Pushing up changes for now

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const data = require('./db/db.json');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3000;
 
 const app = express();
 
@@ -13,21 +13,29 @@ app.use(express.static('public'));
 
 //HTML Routes
 
-//GET /notes should return the notes.html file.
-//testing index.html get request 
+//GET request returning the index.html file.
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//GET /notes should return the notes.html file.
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
-//GET * should return the index.html file.
-//app.get('*', (req, res) => {
-    //res.sendFile(path.join(__dirname, 'index.html'));
-//});
+
 //GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => res.json(data));
+
+//POST /api/notes should receive a new note to save on the request body, 
+//add it to the db.json file, and then return the new note to the client. 
+//You'll need to find a way to give each note a unique id when it's saved 
+//(look into npm packages that could do this for you).
+
+app.post('/api/notes', (req, res) => {
+    res.json(req.body);
+});
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
